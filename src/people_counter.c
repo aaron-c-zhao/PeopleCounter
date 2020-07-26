d21883bac135e019a05bc6b362381610611913ab
Andrea Hu <hjc3299@gmail.com>
2020-07-26 19:38:41 +0200

    Implemented stackBlur by Mario Mario Klingemann

--- src/people_counter.c
+++ src/people_counter.c
@@ -55,7 +55,12 @@ uint8_t detectPeople(ip_mat *frame, ip_mat *background_image, ip_rect *rects)
   // Otherwise we need to make sure that we blur the background only once.
   // Blur(background_image, KERNEL_1);
 
-  gaussianBlur(frame, config.kernel_2);
+  stackBlur(frame->data, config.kernel_1);
+
+#ifdef __TESTING_HARNESS
+  memcpy(th_frame, frame->data, SENSOR_IMAGE_WIDTH * SENSOR_IMAGE_HEIGHT *sizeof(uint8_t));
+#endif 
+
   absDiff(frame, background_image);
   gaussianBlur(frame, config.kernel_3);
   // copy blurred image in case we need to re-apply the threshold.
@@ -65,11 +70,11 @@ uint8_t detectPeople(ip_mat *frame, ip_mat *background_image, ip_rect *rects)
       data[i] = frame->data[i];
   }
   threshold(frame, config.threshold);
-
+/*
 #ifdef __TESTING_HARNESS
   memcpy(th_frame, frame->data, SENSOR_IMAGE_WIDTH * SENSOR_IMAGE_HEIGHT *sizeof(uint8_t));
 #endif 
-
+*/
   /* Blur(frame, config.kernel_4); */
   ip_rect temp_rects[RECTS_MAX_SIZE];
   uint8_t n_rects = findCountours(frame, temp_rects);
@@ -603,6 +608,247 @@ void bubbleSort(ip_closest_centroid *array, uint8_t length)
         array[j] = array[j+1];
         array[j+1] = temp;
       }
+    }
+  }
+}
+
+/* The Stack Blur Algorithm was invented by Mario Klingemann, 
+mario@quasimondo.com and described here:
+http://incubator.quasimondo.com/processing/fast_blur_deluxe.php
+
+Copyright (c) 2010 Mario Klingemann
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+This is the C monochrome (8 bit grayscale) single threaded version
+heavily based on the inplementation by Victor Laskin (victor.laskin@gmail.com)
+More details: http://vitiy.info/stackblur-algorithm-multi-threaded-blur-for-cpp
+*/
+
+static uint16_t const stackblur_mul[255] =
+{
+		512,512,456,512,328,456,335,512,405,328,271,456,388,335,292,512,
+		454,405,364,328,298,271,496,456,420,388,360,335,312,292,273,512,
+		482,454,428,405,383,364,345,328,312,298,284,271,259,496,475,456,
+		437,420,404,388,374,360,347,335,323,312,302,292,282,273,265,512,
+		497,482,468,454,441,428,417,405,394,383,373,364,354,345,337,328,
+		320,312,305,298,291,284,278,271,265,259,507,496,485,475,465,456,
+		446,437,428,420,412,404,396,388,381,374,367,360,354,347,341,335,
+		329,323,318,312,307,302,297,292,287,282,278,273,269,265,261,512,
+		505,497,489,482,475,468,461,454,447,441,435,428,422,417,411,405,
+		399,394,389,383,378,373,368,364,359,354,350,345,341,337,332,328,
+		324,320,316,312,309,305,301,298,294,291,287,284,281,278,274,271,
+		268,265,262,259,257,507,501,496,491,485,480,475,470,465,460,456,
+		451,446,442,437,433,428,424,420,416,412,408,404,400,396,392,388,
+		385,381,377,374,370,367,363,360,357,354,350,347,344,341,338,335,
+		332,329,326,323,320,318,315,312,310,307,304,302,299,297,294,292,
+		289,287,285,282,280,278,275,273,271,269,267,265,263,261,259
+};
+
+static uint8_t const stackblur_shr[255] =
+{
+		9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17,
+		17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19,
+		19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20,
+		20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21,
+		21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21,
+		21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22,
+		22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
+		22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23,
+		23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+		23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+		23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+		23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+		24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24
+};
+
+
+void stackBlur(uint8_t *src, uint8_t radius)
+{
+  uint16_t x, y, xp, yp, i;
+
+  uint8_t stack[radius * (radius + 2) + 1];
+  uint16_t sp;
+  uint16_t stack_start;
+  uint8_t *stack_ptr;
+
+  uint8_t *src_ptr;
+  uint8_t *dst_ptr;
+
+  uint32_t sum_a;
+  uint32_t sum_in_a;
+  uint32_t sum_out_a;
+
+  uint8_t wm = SENSOR_IMAGE_WIDTH - 1;
+  uint8_t hm = SENSOR_IMAGE_HEIGHT - 1;
+
+  uint16_t div = (radius * 2) + 1;
+  uint16_t mul_sum = stackblur_mul[radius];
+  uint8_t shr_sum = stackblur_shr[radius];
+
+  /* step 1*/
+
+  for (y = 0; y < SENSOR_IMAGE_HEIGHT; ++y)
+  {
+    sum_a = sum_in_a = sum_out_a = 0;
+
+    src_ptr = src + SENSOR_IMAGE_WIDTH * y; // start of line (0,y)
+
+    for (i = 0; i <= radius; i++)
+    {
+      stack_ptr = &stack[i];
+      stack_ptr[0] = src_ptr[0];
+      sum_a += src_ptr[0] * (i + 1);
+      sum_out_a += src_ptr[0];
+    }
+
+    for (i = 1; i <= radius; i++)
+    {
+      if (i <= wm)
+      {
+        ++src_ptr;
+      }
+      stack_ptr = &stack[(i + radius)];
+      stack_ptr[0] = src_ptr[0];
+      sum_a += src_ptr[0] * (radius + 1 - i);
+      sum_in_a += src_ptr[0];
+    }
+
+    sp = radius;
+    xp = radius;
+    if (xp > wm)
+    {
+      xp = wm;
+    } 
+    src_ptr = src + (xp + y * SENSOR_IMAGE_WIDTH); //   img.pix_ptr(xp, y);
+    dst_ptr = src + y * SENSOR_IMAGE_WIDTH;            // img.pix_ptr(0, y);
+    for (x = 0; x < SENSOR_IMAGE_WIDTH; x++)
+    {
+      dst_ptr[0] = (sum_a * mul_sum) >> shr_sum;
+      ++dst_ptr;
+
+      sum_a -= sum_out_a;
+
+      stack_start = sp + div - radius;
+      if (stack_start >= div)
+        stack_start -= div;
+      stack_ptr = &stack[stack_start];
+      
+      sum_out_a -= stack_ptr[0];
+
+      if (xp < wm)
+      {
+        ++src_ptr;
+        ++xp;
+      }
+
+      stack_ptr[0] = src_ptr[0];
+
+      sum_in_a += src_ptr[0];
+      sum_a += sum_in_a;
+
+      ++sp;
+      if (sp >= div)
+      {
+        sp = 0;
+      }
+      stack_ptr = &stack[sp];
+
+      sum_out_a += stack_ptr[0];
+      sum_in_a -= stack_ptr[0];
+    }
+  }
+
+  /* step 2 */
+
+  for (x = 0; x < SENSOR_IMAGE_WIDTH; ++x)
+  {
+    sum_a = sum_in_a = sum_out_a = 0;
+
+    src_ptr = src + x; // x,0
+    for (i = 0; i <= radius; i++)
+    {
+      stack_ptr = &stack[i];
+      stack_ptr[0] = src_ptr[0];
+      sum_a += src_ptr[0] * (i + 1);
+      sum_out_a += src_ptr[0];
+    }
+    for (i = 1; i <= radius; i++)
+    {
+      if (i <= hm)
+      {
+        src_ptr += SENSOR_IMAGE_WIDTH; // +stride
+      }
+
+      stack_ptr = &stack[(i + radius)];
+      stack_ptr[0] = src_ptr[0];
+      sum_a += src_ptr[0] * (radius + 1 - i);
+      sum_in_a += src_ptr[0];
+    }
+
+    sp = radius;
+    yp = radius;
+    if (yp > hm)
+    {
+      yp = hm;
+    }
+    src_ptr = src + (x + yp * SENSOR_IMAGE_WIDTH); // img.pix_ptr(x, yp);
+    dst_ptr = src + x;                             // img.pix_ptr(x, 0);
+    for (y = 0; y < SENSOR_IMAGE_HEIGHT; y++)
+    {
+      dst_ptr[0] = (sum_a * mul_sum) >> shr_sum;
+      dst_ptr += SENSOR_IMAGE_WIDTH;
+
+      sum_a -= sum_out_a;
+
+      stack_start = sp + div - radius;
+      if (stack_start >= div)
+      {
+        stack_start -= div;
+      }
+      stack_ptr = &stack[stack_start];
+
+      sum_out_a -= stack_ptr[0];
+
+      if (yp < hm)
+      {
+        src_ptr += SENSOR_IMAGE_WIDTH; // stride
+        ++yp;
+      }
+
+      stack_ptr[0] = src_ptr[0];
+
+      sum_in_a += src_ptr[0];
+      sum_a += sum_in_a;
+
+      ++sp;
+      if (sp >= div)
+      {
+        sp = 0;
+      }
+      stack_ptr = &stack[sp];
+
+      sum_out_a += stack_ptr[0];
+      sum_in_a -= stack_ptr[0];
     }
   }
 }
