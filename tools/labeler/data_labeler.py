cb4dee6b098d086e9347626a77d168c2be98f063
Andrea Hu <hjc3299@gmail.com>
2020-08-04 17:53:26 +0200

    Updated the comments of the labeler

--- tools/labeler/data_labeler.py
+++ tools/labeler/data_labeler.py
@@ -8,15 +8,11 @@ import cv2
 from tools.labeler.raw_data_converter import RawDataConverter
 
 """
-1. Look at each TODO and modify the variables to your needs
-2. Run the program
-3. Press space (any key != from 'e' or Esc) to skip current frame
-4. Press 'e' to save frame to test set and input the label (number of people).
-   The console will show you the current number of frame there are for each label. (so you can make a balanced test set)
-5. Press Esc to quit the program and save to file.
-
-This program will automatically load the export file if it already exists and create a new one from scratch if not.
-This allows you to close and save and come back when needed.
+1. Change the video you want to label (marked by the TODO)
+2. Run the program (data_labeler.py)
+3. Insert the number of people in this frame and confirm (the last number will be in the text field by default) 
+4. Insert the count of people in the room and confirm (the last number will be in the text field by default)
+5. Continue until all frames are labeled (you can keep pressing enter if no changes are to be made to labels)
 """
 
 
@@ -26,7 +22,7 @@ def ask_label(n):
 
 
 def ask_count(n):
-    s = simpledialog.askinteger("Count label", "Insert the number of people in the room", initialvalue=n)
+    s = simpledialog.askinteger("Count label", "Insert the count of people in the room", initialvalue=n)
     return n if s is None else s
 
 
@@ -45,7 +41,6 @@ result = {"num_people": [], "room_count": []}  # if not os.path.isfile(export_fi
 ROOT = tk.Tk()
 ROOT.withdraw()
 cv2.namedWindow("image")
-# cv2.resizeWindow("image", 320, 240)
 
 try:
     while True:
@@ -71,13 +66,6 @@ try:
 
         if key == 27:  # Esc key to stop
             break
-        # elif key == ord('e'):  # e to add this frame to test dataset
-        #     print("Current number of frames per label = ", counter)
-        #     n_people = ask_label()  # ask for number of people in frame
-        #     counter[n_people] += 1
-        #     result["frames"].append(rdc.raw_frame)
-        #     result["labels"].append(n_people)
-        #     continue
 
 except FileNotFoundError:
     print("Error! Your progress will be saved.")
